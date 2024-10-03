fee03ec887bc8c1823ebb60729d26b44a1238651
choque9 <choque.9@gmail.com>
2024-10-03 18:10:29 -0500

    Add request intercept to add_time

--- KI-1089066.js
+++ KI-1089066.js
@@ -1,4 +1,39 @@
 $(document).ready(function () {
+
+    // Case 1: Intercepts the add_time request and replaces it with GIFT criteria
+    const originalXHRSend = XMLHttpRequest.prototype.send;
+    const originalXHROpen = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function (method, url, ...rest) {
+        this._requestUrl = url;
+        return originalXHROpen.apply(this, [method, url, ...rest]);
+    };
+
+    XMLHttpRequest.prototype.send = function (body) {
+
+        const urlPattern = /\/api\/checkout\/pub\/orderForm\/.*\/itemsOrdination/;
+
+        if (this._requestUrl && urlPattern.test(this._requestUrl)) {
+            let requestBody;
+
+            try {
+                requestBody = JSON.parse(body);
+            } catch (error) {
+                requestBody = null;
+            }
+
+            if (requestBody && requestBody.criteria === "add_time") {
+                console.log('Modifying requestBody to avoid add_time criteria: changed to GIFT criteria');
+                window.checkout.loading(false);
+                $('body').removeClass('loading');
+                requestBody.criteria = "GIFT";
+                body = JSON.stringify(requestBody);
+            }
+        }
+        return originalXHRSend.apply(this, [body]);
+    };
+
+    // Case 2: Listener to sort by GIFT on payment step
+
     let listenerCreated = false;
     const observerContainer = new MutationObserver((mutations, obsP) => {
         const paymentDataContainer = document.querySelector("#payment-data");
--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # KI-1089066
 [KI] Nominal Taxes + Gift Promotions on the same item might generate checkout errors
 
-
 This script is a workaround to KI https://vtexhelp.zendesk.com/agent/tickets/1089066.
 
 You need to add the script to the checkout at the checkout6-custom.js
 
 
-This script change items ordination by "GIFT"  in payment step when coupon and taxes exists. 
+There are two cases where it is necessary to sort by GIFT in order to allow create the order.
+- *Case 1 - When there are more than 20 items in the orderform*: vcs.checkout-ui performs a sort by 'add_time' criteria. When this sorting condition exist, it does not allow to reorder by GIFT criteria. 
+This solution intercepts the “add_time” request and replaces by GIFT criteria. The script must be added in the checkout customization ensuring that it is inserted when the DOM is loaded, before the first add_time sort is performed. 
+- *Case 2 - Before closing the order at checkout*: this script change items ordination by "GIFT" criteria in payment step when coupon and taxes exists.
+
 
 ```
     vtexjs.checkout
