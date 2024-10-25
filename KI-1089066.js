9141deedd9b1d7776526ce90eb060bbefe277f1b
choque9 <choque.9@gmail.com>
2024-10-24 21:46:34 -0500

    Update algorithm. Only request sorting of items at the end of PaymentData and ShippingData requests

--- KI-1089066.js
+++ KI-1089066.js
@@ -1,81 +1,80 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-    // Case 1: Intercepts the add_time request and replaces it with GIFT criteria
-    const originalXHRSend = XMLHttpRequest.prototype.send;
-    const originalXHROpen = XMLHttpRequest.prototype.open;
-    XMLHttpRequest.prototype.open = function (method, url, ...rest) {
-        this._requestUrl = url;
-        return originalXHROpen.apply(this, [method, url, ...rest]);
-    };
+	interceptRequest_KI_1089066()
 
-    XMLHttpRequest.prototype.send = function (body) {
-
-        const urlPattern = /\/api\/checkout\/pub\/orderForm\/.*\/itemsOrdination/;
-
-        if (this._requestUrl && urlPattern.test(this._requestUrl)) {
-            let requestBody;
-
-            try {
-                requestBody = JSON.parse(body);
-            } catch (error) {
-                requestBody = null;
-            }
-
-            if (requestBody && requestBody.criteria === "add_time") {
-                console.log('Modifying requestBody to avoid add_time criteria: changed to GIFT criteria');
-                window.checkout.loading(false);
-                $('body').removeClass('loading');
-                requestBody.criteria = "GIFT";
-                body = JSON.stringify(requestBody);
-            }
-        }
-        return originalXHRSend.apply(this, [body]);
-    };
-
-    // Case 2: Listener to sort by GIFT on payment step
-
-    let listenerCreated = false;
-    const observerContainer = new MutationObserver((mutations, obsP) => {
-        const paymentDataContainer = document.querySelector("#payment-data");
-        if (document.contains(paymentDataContainer)) {
-            if (!listenerCreated) {
-                listenerCreated = true;
-                createListener(listenerCreated);
-            }
-        } else {
-            listenerCreated = false;
-        }
-    });
-
-    observerContainer.observe(document, {
-        childList: true,
-        subtree: true,
-    });
 });
 
-function createListener(listenerFlag) {
-    if (listenerFlag) {
-        function checkURL() {
-            if (window.location.hash.includes('/payment')) {
-                orderItems();
-            }
-        }
+function changeItemsOrdination() {
+	if ((vtexjs.checkout.orderForm.marketingData !== null &&
+			vtexjs.checkout.orderForm.marketingData.coupon !== null &&
+			vtexjs.checkout.orderForm.totalizers !== null &&
+			vtexjs.checkout.orderForm.totalizers.some(item => item.id === "Tax")) ||
+		vtexjs.checkout.orderForm.items.length > 20) {
+		vtexjs.checkout
+			.changeItemsOrdination("GIFT")
+			.then(function(orderForm) {
+				console.log("items sorted by criteria: ", orderForm.itemsOrdination)
+			})
+	}
+}
 
-        function orderItems() {
-            if ((vtexjs.checkout.orderForm.marketingData !== null
-                && vtexjs.checkout.orderForm.marketingData.coupon !== null
-                && vtexjs.checkout.orderForm.totalizers !== null
-                && vtexjs.checkout.orderForm.totalizers.some(item => item.id === "Tax"))
-                || vtexjs.checkout.orderForm.items.length > 20) {
-                vtexjs.checkout
-                    .changeItemsOrdination("GIFT")
-                    .then(function (orderForm) {
-                        console.log("items sorted by criteria: ", orderForm.itemsOrdination)
-                    })
-            }
-        }
+function interceptRequest_KI_1089066() {
 
-        window.addEventListener('popstate', checkURL);
-        checkURL();
-    }
+	const originalXHRSend = XMLHttpRequest.prototype.send;
+	const originalXHROpen = XMLHttpRequest.prototype.open;
+	XMLHttpRequest.prototype.open = function(method, url, ...rest) {
+		this._requestUrl = url;
+		return originalXHROpen.apply(this, [method, url, ...rest]);
+	};
+
+	XMLHttpRequest.prototype.send = function(body) {
+		const xhr = this;
+		const urlPatternItemsOrdination = /\/api\/checkout\/pub\/orderForm\/.*\/itemsOrdination/;
+		const urlPatternPaymentData = /\/api\/checkout\/pub\/orderForm\/.*\/attachments\/paymentData/;
+		const urlPatternShippingData = /\/api\/checkout\/pub\/orderForm\/.*\/attachments\/shippingData/;
+
+		if (this._requestUrl && urlPatternItemsOrdination.test(this._requestUrl)) {
+			let requestBody;
+
+			try {
+				requestBody = JSON.parse(body);
+			} catch (error) {
+				requestBody = null;
+			}
+
+			if (requestBody && requestBody.criteria === "add_time") {
+				console.log('Modifying requestBody to avoid add_time criteria: changed to GIFT criteria');
+				window.checkout.loading(false);
+				$('body').removeClass('loading');
+				requestBody.criteria = "GIFT";
+				body = JSON.stringify(requestBody);
+			}
+		}
+
+		if (this._requestUrl &&
+			(urlPatternPaymentData.test(this._requestUrl) ||
+				urlPatternShippingData.test(this._requestUrl))) {
+			return new Promise((resolve, reject) => {
+				xhr.onload = function() {
+					if (xhr.status >= 200 && xhr.status < 300) {
+						resolve(xhr.responseText);
+					} else {
+						reject(new Error("Request failed  by custom script with status: " + xhr.status));
+					}
+				};
+
+				xhr.onerror = function() {
+					reject(new Error("Network error by custom script"));
+				};
+
+				originalXHRSend.apply(xhr, [body]);
+			}).then((_response) => {
+				changeItemsOrdination();
+			}).catch((error) => {
+				console.error("Error during the request by custom script:", error);
+			});
+		} else {
+			return originalXHRSend.apply(this, [body]);
+		}
+	};
 }
--- README.md
+++ README.md
@@ -5,11 +5,17 @@ This script is a workaround to KI https://vtexhelp.zendesk.com/agent/tickets/108
 
 You need to add the script to the checkout at the checkout6-custom.js
 
+## Functions 
+### Change Items Ordination
 
-There are two cases where it is necessary to sort by GIFT in order to allow create the order.
-- *Case 1 - When there are more than 20 items in the orderform*: vcs.checkout-ui performs a sort by 'add_time' criteria. When this sorting condition exist, it does not allow to reorder by GIFT criteria. 
+It is necessary to sort by GIFT in order to allow create the order. The request for item ordering will only be requested if one of the conditions is met:
+- When the length of items is greater than 20: vcs.checkout-ui performs a sort by 'add_time' criteria. When this sorting condition exist, it does not allow to reorder by GIFT criteria. 
 This solution intercepts the “add_time” request and replaces by GIFT criteria. The script must be added in the checkout customization ensuring that it is inserted when the DOM is loaded, before the first add_time sort is performed. 
-- *Case 2 - Before closing the order at checkout*: this script change items ordination by "GIFT" criteria in payment step when coupon and taxes exists, Or when the length of items is greater than 20
+- When coupon and taxes exists
+  
+###  Intercept requests
+- Case 1: Intercepts the itemsOrdination request by 'add_time' criteria and replaces it with 'GIFT' criteria
+- Case 2: Intercepts PaymentData and ShippingData request. At the end of each of the requests, it performs a ChangeItemsOrdination()
 
 
 ```
